067d823cf5fe3b5c4097ee8d928b047c8c228713
yoshimiyu <yoshiyukimihara@Yoshiyukis-MacBook-Air.local>
2025-07-08 00:43:31 +0900

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'suitoucho-cache-v6'; // キャッシュ名を変更して新しいキャッシュを強制
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-180x180-v2.png',
+  './icons/icon-192x192-v2.png',
+  './icons/icon-512x512-v2.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
