2f44dfcb29a1471be26941f94f25be62ac5141de
yoshimiyu <yoshiyukimihara@Yoshiyukis-MacBook-Air.local>
2025-07-08 00:50:00 +0900

    Fix: Icon paths in index.html, manifest.json, and service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,9 @@ const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-180x180-v2.png',
-  './icons/icon-192x192-v2.png',
-  './icons/icon-512x512-v2.png',
+  './icons/icon-180x180.png',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
 ];
